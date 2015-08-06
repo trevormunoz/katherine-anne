c06c36dd5daccc955fa9c57f87f0eccd743139cb
trevormunoz <trevor.munoz@gmail.com>
2015-08-06 17:47:17 -0400

    Enqueue more scripts---for typography

--- src/functions.php
+++ src/functions.php
@@ -11,9 +11,9 @@
    remove_action( 'wp_head',             'rsd_link'                               );
    remove_action( 'wp_head',             'wlwmanifest_link'                       );
    remove_action( 'wp_head',             'print_emoji_detection_script',     7    );
-   remove_action( 'wp_head',             'wp_print_head_scripts',            9    );
    remove_action( 'wp_head',             'rel_canonical'                          );
    remove_action( 'wp_head',             'wp_site_icon',                    99    );
+  //  remove_action( 'wp_footer',           'wp_print_footer_scripts',         20    );
  }
  add_action( 'init', 'modify_wp_head' );
 
@@ -35,6 +35,31 @@
      );
  }
  add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+ function theme_modify_head_scripts() {
+   wp_register_script( 'modernizr', get_stylesheet_directory_uri() . '/js/libs/modernizr.js',
+      array(), '2.8.3', false );
+    wp_register_script( 'typography', get_stylesheet_directory_uri() . '/js/typography.js',
+      array('jquery'), '0.7.0', true );
+
+   wp_enqueue_script( 'modernizr' );
+   wp_enqueue_script( 'typography' );
+ }
+ add_action( 'wp_enqueue_scripts', 'theme_modify_head_scripts' );
+
+ function theme_typekit_fonts() {
+   wp_register_script( 'theme_typekit', '//use.typekit.net/kyi0gmx.js', array(), '', false );
+   wp_enqueue_script( 'theme_typekit' );
+ }
+ add_action( 'wp_enqueue_scripts', 'theme_typekit_fonts' );
+
+ function theme_typekit_inline() {
+   if ( wp_script_is( 'theme_typekit', 'done' ) ) { ?>
+     <script type="text/javascript">try{Typekit.load({ async: true });}catch(e){}</script>
+    <?php
+  }
+ }
+add_action( 'wp_head', 'theme_typekit_inline' );
 
  function kap_remove_page_templates( $templates ) {
    unset( $templates['page-templates/archive.php'] );
---
+++ src/js/typography.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($) {
+  $(".site-main-title").lettering('words');
+  $(".site-sub-title").lettering('words');
+});
