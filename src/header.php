f5ed9830e747dc2b9c5a1d091a31baccd5c7a751
trevormunoz <trevor.munoz@gmail.com>
2015-08-09 20:41:02 -0400

    Don't output menu div on front page

--- src/header.php
+++ src/header.php
@@ -18,22 +18,19 @@
 <body>
 
 	<!-- main nav -->
-	<?php
-	if ( has_nav_menu( 'primary' ) ) : ?>
-	<div id="secondary" class="secondary">
-
-		<?php if ( ! is_home() && has_nav_menu( 'primary' ) ) : ?>
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<?php
-					// Primary navigation menu.
-					wp_nav_menu( array(
-						'menu_class'     => 'nav-menu',
-						'theme_location' => 'primary',
-					) );
-				?>
-			</nav><!-- .main-navigation -->
-		<?php endif; ?>
-
-	</div><!-- .secondary -->
-
-<?php endif; ?>
+	<?php if ( ! is_home() && has_nav_menu( 'primary' ) ) : ?>
+			<div id="secondary" class="secondary clearfix">
+				<div class="site-logo">
+					<a href="introduction">Home</a>
+				</div><!-- .site-logo -->
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<?php
+						// Primary navigation menu.
+						wp_nav_menu( array(
+							'menu_class'     => 'nav-menu',
+							'theme_location' => 'primary',
+						) );
+					?>
+				</nav><!-- .main-navigation -->
+		</div><!-- .secondary -->
+	<?php endif; ?>
