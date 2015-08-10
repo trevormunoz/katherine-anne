2e96eb623a7574fd3a760286dfac4d7508c8bd85
trevormunoz <trevor.munoz@gmail.com>
2015-08-09 20:41:42 -0400

    adjust size and layout of columns on landing page

--- src/index.php
+++ src/index.php
@@ -9,8 +9,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<div class="cover">
-			<div class="small-7 small-centered large-offset-1 large-uncentered columns">
+			<div class="small-6 small-centered large-offset-1 large-uncentered columns">
 				<h1 class="site-main-title">
 					Katherine <br />
 					Anne <br />
@@ -18,14 +17,13 @@ get_header(); ?>
 				</h1>
 				<span class="site-sub-title">correspondence</span>
 			</div>
-			<div class="small-4 columns">
+			<div class="small-4 end columns">
 				<p class="intro">Over 5,000 pages of digitized materials granting unprecedented access
 					 to the ideas, attitudes, and experiences of a distinguished
 					 20th century American writer.
 				 </p>
 				 <a href="introduction" class="button">Read More</a>
 			</div>
-		</div>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
