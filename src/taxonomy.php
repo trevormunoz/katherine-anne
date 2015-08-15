68473f51f3e484320a7ca35421efb3a1a25db453
trevormunoz <trevor.munoz@gmail.com>
2015-08-15 11:02:09 -0400

    New child theme files for term and custom post archives and singletons

---
+++ src/taxonomy.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * If you'd like to further customize these archive views, you may create a
+ * new template file for each one. For example, tag.php (Tag archives),
+ * category.php (Category archives), author.php (Author archives), etc.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage KAP Correspondence
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+			<div class="archive-header-container"
+				<header class="archive-header">
+					<h1 class="archive-title">
+						<?php
+							single_term_title();
+							the_archive_description( '<small class="archive-description">', '</small>' );
+						?>
+					</h1>
+				</header><!-- .archive-header -->
+			</div><!-- .archive-header-container -->
+			<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
+
+			// End the loop.
+			endwhile;
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+
+<?php get_footer(); ?>
