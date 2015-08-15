92dc767f1b2cad79d4aca396e1049da52f9d4485
trevormunoz <trevor.munoz@gmail.com>
2015-08-15 11:02:48 -0400

    fix home link

--- src/header.php
+++ src/header.php
@@ -21,7 +21,7 @@
 	<?php if ( ! is_home() && has_nav_menu( 'primary' ) ) : ?>
 			<div id="secondary" class="secondary clearfix">
 				<div class="site-logo">
-					<a href="introduction">Home</a>
+					<a href="/introduction">Home</a>
 				</div><!-- .site-logo -->
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php
