4b6c5ad5d647759c2498a04810ea26c57f8139de
trevormunoz <trevor.munoz@gmail.com>
2015-08-27 07:53:44 -0600

    Markup changes to accommodate shift to Bootstrap

--- src/header.php
+++ src/header.php
@@ -7,7 +7,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
@@ -19,18 +19,20 @@
 
 	<!-- main nav -->
 	<?php if ( ! is_home() && has_nav_menu( 'primary' ) ) : ?>
-			<div id="secondary" class="secondary clearfix">
-				<div class="site-logo">
-					<a href="/introduction">Home</a>
-				</div><!-- .site-logo -->
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<?php
-						// Primary navigation menu.
-						wp_nav_menu( array(
-							'menu_class'     => 'nav-menu',
-							'theme_location' => 'primary',
-						) );
-					?>
-				</nav><!-- .main-navigation -->
+			<div id="secondary" class="secondary">
+				<div class="site-menu">
+					<div class="site-logo">
+						<a href="/introduction">Home</a>
+					</div><!-- .site-logo -->
+					<nav id="site-navigation" class="site-nav" role="navigation">
+						<?php
+							// Primary navigation menu.
+							wp_nav_menu( array(
+								'menu_class'     => 'nav nav-pills nav-inline',
+								'theme_location' => 'primary',
+							) );
+						?>
+					</nav><!-- .main-navigation -->
+				</div><!-- .menu -->
 		</div><!-- .secondary -->
 	<?php endif; ?>
--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<div class="small-6 small-centered large-offset-1 large-uncentered columns">
+			<div class="site-title-container p-a-lg">
 				<h1 class="site-main-title">
 					Katherine <br />
 					Anne <br />
@@ -17,12 +17,12 @@ get_header(); ?>
 				</h1>
 				<span class="site-sub-title">correspondence</span>
 			</div>
-			<div class="small-4 end columns">
+			<div class="site-call-to-action p-a-lg">
 				<p class="intro">Over 5,000 pages of digitized materials granting unprecedented access
 					 to the ideas, attitudes, and experiences of a distinguished
 					 20th century American writer.
 				 </p>
-				 <a href="introduction" class="button">Read More</a>
+				 <a href="introduction" class="btn main-action-button">Read More</a>
 			</div>
 
 		</main><!-- .site-main -->
