50cac91d79fc424499c45965452a8a9703b4ebc8
trevormunoz <trevor.munoz@gmail.com>
2015-09-24 09:44:58 -0400

    Add template for static front page

---
+++ src/front-page.php
@@ -0,0 +1,31 @@
+<?php
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<section class="site-hero-section">
+			  <div class="site-title-container p-a-lg">
+			    <h1 class="site-main-title">
+			      Katherine <br />
+			      Anne <br />
+			      Porter<br />
+			    </h1>
+			    <span class="site-sub-title">correspondence</span>
+			  </div>
+			</section>
+			<section class="site-about-teaser">
+			  <div class="site-call-to-action p-a-lg">
+			    <p class="intro">Over 5,000 pages of digitized materials granting unprecedented access
+			       to the ideas, attitudes, and experiences of a distinguished
+			       20th century American writer.
+			     </p>
+			     <a href="introduction" class="btn main-action-button">Read More</a>
+			  </div>
+			</section>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
