dce46275c6c82bdf4f550619af6c83cb8879fa30
trevormunoz <trevor.munoz@gmail.com>
2015-10-03 16:27:29 -0400

    Reorganize front page markup to include a hero section

--- src/front-page.php
+++ src/front-page.php
@@ -1,27 +1,23 @@
 <?php
 
 get_header(); ?>
-
+	<div id="splash" class="hero-area">
+		<div class="site-hero-title">
+			<h1 class="site-main-title"><span>Katherine</span> <span>Anne</span> <span>Porter</span></h1>
+			<span class="site-sub-title">correspondence</span>
+		</div><!-- .site-hero-title -->
+	</div><!-- .hero-area -->
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="site-hero-section">
-			  <div class="site-title-container p-a-lg">
-			    <h1 class="site-main-title"><span>Katherine</span> <span>Anne</span> <span>Porter</span></h1>
-			    <span class="site-sub-title">correspondence</span>
-			  </div>
-			</section>
-			<section class="site-about-teaser">
-			  <div class="site-call-to-action p-a-lg">
-			    <p class="intro">Over 5,000 pages of digitized materials granting unprecedented access
-			       to the ideas, attitudes, and experiences of a distinguished
-			       20th century American writer.
-			     </p>
-			     <a href="introduction" class="btn main-action-button">Read More</a>
-			  </div>
-			</section>
-
-		</main><!-- .site-main -->
+		<section class="site-about-teaser">
+		  <div class="site-call-to-action">
+		    <p class="intro">The University of Maryland Libraries are pleased to be
+					able to share over 5,000 pages of digitized materials granting
+					unprecedented access to the ideas, attitudes, and experiences of a
+					distinguished 20th century American writer.
+		     </p>
+		     <a href="introduction" class="btn main-action-button">Read More</a>
+		  </div>
+		</section><!-- .site-call-to-action -->
 	</div><!-- .content-area -->
 
 <?php get_footer(); ?>
