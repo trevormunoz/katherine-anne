34e8b91a3a56d4ff74fa9489ecfa5a8f0115e78f
trevormunoz <trevor.munoz@gmail.com>
2015-10-03 22:53:34 -0400

    Add dummy placeholder text to front page

--- src/front-page.php
+++ src/front-page.php
@@ -22,12 +22,15 @@ get_header(); ?>
 		<div class="browse-options">
 			<section class="browse-teaser">
 				<h2><small>Browse by</small> Correspondent</h2>
+				<p>Listicle literally pour-over street art quinoa scenester. Mumblecore fap Blue Bottle narwhal craft beer, Bushwick keytar +1 Carles High Life fingerstache Etsy cronut locavore mixtape. Etsy messenger bag Echo Park, Pitchfork whatever Tumblr cliche brunch hashtag blog.</p>
 			</section>
 			<section class="browse-teaser">
 				<h2><small>Browse by</small> Location</h2>
+				<p>Schlitz tofu skateboard Carles. Quinoa Truffaut lomo, sartorial fashion axe iPhone art party fap salvia.</p>
 			</section>
 			<section class="browse-teaser">
 				<h2><small>Browse by</small> Decade</h2>
+				<p>Whatever Vice DIY, Godard drinking vinegar mustache meh Echo Park synth semiotics yr selfies. Chia slow-carb whatever organic, chillwave paleo food truck typewriter gluten-free umami four dollar toast Banksy actually hella. Before they sold out taxidermy butcher plaid Godard meggings, Schlitz selvage seitan banh mi slow-carb stumptown mustache jean shorts authentic. VHS readymade ugh narwhal occupy fanny pack.</p>
 			</section>
 		</div><!-- .browse-options -->
 		<hr />
@@ -42,6 +45,8 @@ get_header(); ?>
 		<hr />
 		<section id="more-about" class="project-about-teaser">
 			<h2>About the Project</h2>
+			<p>Dreamcatcher locavore kitsch farm-to-table vinyl Vice. Vice whatever taxidermy bicycle rights drinking vinegar post-ironic. Helvetica kitsch keffiyeh four dollar toast Thundercats wolf kale chips cold-pressed before they sold out. Pork belly Thundercats photo booth skateboard, banh mi occupy Tumblr keytar meh keffiyeh sriracha plaid next level. Chia flannel Wes Anderson, stumptown 8-bit tofu selvage High Life fap Austin tilde. Direct trade PBR&B selfies fingerstache, normcore fap 90's. Next level kitsch ennui, mlkshk artisan fixie distillery heirloom XOXO Marfa cred.</p>
+			<p>Fingerstache meditation YOLO single-origin coffee. Whatever mixtape irony fanny pack normcore single-origin coffee, Odd Future disrupt. Banksy meggings gentrify, vegan meditation craft beer gluten-free Tumblr plaid Shoreditch locavore iPhone Pinterest Etsy direct trade. Pinterest bitters Odd Future keytar. Vegan master cleanse Carles, +1 slow-carb selfies lumbersexual artisan banjo bitters Bushwick jean shorts. Pour-over plaid cred raw denim, ethical tilde Carles literally umami paleo Shoreditch. Single-origin coffee forage Shoreditch, +1 synth tofu meh whatever master cleanse YOLO Blue Bottle blog banjo.</p>
 		</section>
 	</div><!-- .content-area -->
 
