e7b7e748077c45333b678174659d37f398ca740b
trevormunoz <trevor.munoz@gmail.com>
2015-11-02 14:05:13 -0500

    Add template for displaying search results

---
+++ src/search.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying search results pages.
+ *
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area search">
+		<main id="main" class="site-main" role="main">
+
+
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+
+<?php get_footer(); ?>
