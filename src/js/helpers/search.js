cc060423e1637fd83e3a8b04b403fba2012dc48e
trevormunoz <trevor.munoz@gmail.com>
2015-12-05 12:11:50 -0500

    Upgrade to Elasticsearch 2.0

--- src/js/helpers/search.js
+++ src/js/helpers/search.js
@@ -2,7 +2,7 @@ import * as elasticsearch from 'elasticsearch';
 
 const searchClient = new elasticsearch.Client({
   host: 'http://192.168.33.11:9200',
-  apiVersion: '1.7'
+  apiVersion: '2.0'
   //, log: 'trace'
 });
 
