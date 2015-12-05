4372efc13e77b7c57a2b089ed48dead1128d3898
trevormunoz <trevor.munoz@gmail.com>
2015-12-05 12:51:36 -0500

    Actually start executing the search

--- src/js/views/searchResults-view.js
+++ src/js/views/searchResults-view.js
@@ -1,14 +1,33 @@
 import $ from 'jquery';
 import * as Backbone from 'backbone';
+import searchClient from '../helpers/search.js';
 
 class SearchResultsView extends Backbone.View {
 
   get className() { return 'results'; }
 
   initialize(options) {
-    this.options = options;
-    window.console.log('SearchResultsView initialized!');
-    window.console.log(this.options.query);
+    let query = options.query.substring(2);
+    let queryObj = {
+      query: {
+        template: {
+          file: 'base_query',
+          params: {
+            query_string: query
+          }
+        }
+      }
+    };
+
+    searchClient.search({
+      index: 'kap',
+      type: 'item',
+      body: queryObj
+    }, function(err, response) {
+      if(!err) {
+        window.console.log(response.hits);
+      }
+    })
   }
 
 
