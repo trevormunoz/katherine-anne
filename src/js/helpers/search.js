ecc5bcdb43cf5b1a0f624829f5693bfef5303b1b
trevormunoz <trevor.munoz@gmail.com>
2015-12-11 17:44:50 -0500

    Create a new class to encapsulate fetching data via search

---
+++ src/js/helpers/search.js
@@ -0,0 +1,49 @@
+import * as _ from 'underscore';
+import searchClient from './client';
+
+class DocumentSet {
+
+  constructor(response) {
+    this.hits = response.hits;
+    this.facets = response.aggregations;
+  }
+
+  get totalHits() { return this.hits.total; }
+
+  get documents() {
+    return this.returnDocuments();
+  }
+
+  static search(query) {
+    const queryRegex = /^["']/;
+    let queryTemplate = queryRegex.test(query) ?
+      'faceted_phrase_query' : 'faceted_base_query';
+
+    let queryObj = {
+      template: queryTemplate,
+      params : {
+        query_string: query
+      }
+    };
+
+    let searchPromise = searchClient.searchTemplate({
+      index: 'kap',
+      sort: '_doc',
+      body: queryObj
+    });
+
+    return searchPromise;
+  }
+
+  calculatePages(pageSize = 20) {
+    return Math.ceil(this.totalHits % pageSize);
+  }
+
+  returnDocuments() {
+    let documents = this.hits.hits;
+    return _.map(documents, (doc) => { return doc._source; });
+  }
+
+}
+
+export default DocumentSet;
