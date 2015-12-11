829a5f2190c709ec6cf6d5353cd8f4f07498240a
trevormunoz <trevor.munoz@gmail.com>
2015-12-11 17:42:42 -0500

    Simplify SearchResults View
    * greater isolation of concerns if this view isn’t also responsible for
    running the search

--- src/js/views/searchResults-view.js
+++ src/js/views/searchResults-view.js
@@ -1,35 +1,12 @@
-import $ from 'jquery';
 import * as Backbone from 'backbone';
-import searchClient from '../helpers/search.js';
 
 class SearchResultsView extends Backbone.View {
 
   get className() { return 'results'; }
 
-  initialize(options) {
-    let query = options.query.substring(2);
-    let queryObj = {
-      query: {
-        template: {
-          file: 'base_query',
-          params: {
-            query_string: query
-          }
-        }
-      }
-    };
-
-    searchClient.search({
-      index: 'kap',
-      type: 'item',
-      body: queryObj
-    }, function(err, response) {
-      if(!err) {
-        window.console.log(response.hits);
-      }
-    })
+  render() {
+    window.console.log(this.collection);
   }
-
 
 }
 
