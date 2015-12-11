f2591c10c6483cad0800a4653bb45d7671ab2fbc
trevormunoz <trevor.munoz@gmail.com>
2015-12-11 10:26:05 -0500

    Rename elasticsearch singleton

--- src/js/helpers/client.js
+++ src/js/helpers/client.js
@@ -2,7 +2,7 @@ import * as elasticsearch from 'elasticsearch';
 
 const searchClient = new elasticsearch.Client({
   host: 'http://192.168.33.11:9200',
-  apiVersion: '2.0'
+  apiVersion: '2.1'
   //, log: 'trace'
 });
 
