a2d9e7b8dcc8e68885ea1b6eacf796f6bddbd093
trevormunoz <trevor.munoz@gmail.com>
2015-12-11 17:45:39 -0500

    Use search class to get data and create an item collection

--- src/js/routers/app-router.js
+++ src/js/routers/app-router.js
@@ -1,6 +1,8 @@
 import * as Backbone from 'backbone';
+import Items from '../collections/items';
 import SearchBoxView from '../views/searchBox-view';
 import SearchResultsView from '../views/searchResults-view';
+import DocumentSet from '../helpers/search';
 import dispatcher from '../helpers/dispatcher';
 
 class AppRouter extends Backbone.Router {
@@ -25,7 +27,13 @@ class AppRouter extends Backbone.Router {
   }
 
   showSearchResults(queryString) {
-    new SearchResultsView({query: queryString});
+    let q = queryString.substring(2);
+
+    DocumentSet.search(q).then(function(result) {
+      let currentDocuments = new DocumentSet(result).documents;
+      let docCollection = new Items(currentDocuments);
+      new SearchResultsView({collection: docCollection}).render();
+    });
   }
 
 }
