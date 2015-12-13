e8ddc410cd64edb0b2b96dc103e22923446081b5
trevormunoz <trevor.munoz@gmail.com>
2015-12-13 18:09:38 -0500

    Create a base view for other views to inherit
    * Provide some lifecycle methods for subview management

---
+++ src/js/views/_base-view.js
@@ -0,0 +1,23 @@
+import * as _ from 'underscore';
+import * as Backbone from 'backbone';
+
+class BaseView extends Backbone.View {
+
+  constructor() {
+    super();
+    this._subviews = null;
+  }
+
+  registerSubView(view) {
+    this._subviews = this._subviews || [];
+    this._subviews.push(view);
+  }
+
+  remove() {
+    _.invoke(this._subviews, 'remove');
+    Backbone.View.prototype.remove.call(this);
+  }
+
+}
+
+export default BaseView;
