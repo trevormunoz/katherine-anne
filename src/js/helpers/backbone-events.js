d4beccc16c918878831324d686e918f3b4c1d7bd
trevormunoz <trevor.munoz@gmail.com>
2015-12-13 18:08:42 -0500

    Copy the workaround/hack to have access to Backbone.Events

---
+++ src/js/helpers/backbone-events.js
@@ -0,0 +1,3 @@
+import * as Backbone from 'backbone';
+
+export default Backbone.Events;
--- src/js/helpers/dispatcher.js
+++
@@ -1,6 +0,0 @@
-import Backbone from 'backbone';
-import * as _ from 'underscore';
-
-const dispatcher = _.extend({}, Backbone.Events);
-
-export default dispatcher;
