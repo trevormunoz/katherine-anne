a98a85b5fc7c14bc03197c2b7ec0f42da3cf861b
trevormunoz <trevor.munoz@gmail.com>
2015-12-17 17:51:24 -0500

    Let WP handle form submission completely

--- src/js/views/searchBox-view.js
+++ src/js/views/searchBox-view.js
@@ -5,18 +5,10 @@ class SearchBoxView extends Backbone.View {
 
   get el() { return '#search-box'; }
 
-  get events() {
-    return {
-      'submit form': 'sendQuery'
-    }
-  }
-
   get query() {
     return $('#search-box input').val();
   }
 
-  initialize() {
-    this.listenTo(Events, 'search:noQuery', this.handleEmptyQuery);
   }
 
   handleEmptyQuery() {
