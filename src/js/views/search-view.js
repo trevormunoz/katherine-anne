ec9f2b5c8e5a3de778d1398325da6b58f4f55db7
trevormunoz <trevor.munoz@gmail.com>
2015-12-17 17:53:58 -0500

    Rewrite search view to handle search + create subcomponents

--- src/js/views/search-view.js
+++ src/js/views/search-view.js
@@ -1,20 +1,45 @@
+import * as _ from 'underscore';
 import BaseView from './_base-view';
 import SearchBoxView from './searchBox-view';
+import DocumentSet from '../helpers/search';
+import Items from '../collections/items';
+import SearchResultsView from './searchResults-view';
+import Facets from '../collections/facets';
+import SearchFacetView from './searchFacet-view';
 
 class SearchView extends BaseView {
 
-  initialize() {
-    window.console.log('SearchView initialized.');
-    let searchBoxView = new SearchBoxView();
-    this.registerSubView(searchBoxView);
+  initialize(options = {}) {
+    this.options = options;
+    if (!this.options.query) {
+      this.initSearchBox();
+    } else {
+      this.initSearchBox(options);
+      let search = DocumentSet.search(this.options.query);
+      this.handleSearch(search);
+    }
+  }
 
-    this.listenTo(Events, 'search:started', this.handleSearch);
+  initSearchBox(options = {}) {
+    let searchBoxView = new SearchBoxView(options);
+    this.registerSubView(searchBoxView);
   }
 
   handleSearch(searchPromise) {
-    window.console.log('Search result handler fired');
-    searchPromise.then(function(result) {
-      window.console.log(result);
+    searchPromise.then((result) => {
+      let documents = _.map(result.hits.hits, (doc) => { return doc._source; });
+      let items = new Items(documents);
+      let resultsView = new SearchResultsView({collection: items});
+      this.registerSubView(resultsView);
+    });
+
+    searchPromise.then((result) => {
+      let facetNames = _.keys(result.aggregations);
+      _.each(facetNames, (name) => {
+        let facetCollex = new Facets(result.aggregations[name].buckets);
+        let facetView = new SearchFacetView({collection: facetCollex});
+        this.registerSubView(facetView);
+      });
     });
   }
 }
