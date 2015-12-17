a89c45934ca0ac4e1782e9d1d10f2bf5afbd1a22
trevormunoz <trevor.munoz@gmail.com>
2015-12-17 17:45:27 -0500

    Go back to having events bus with custom name

--- src/js/helpers/backbone-events.js
+++
@@ -1,3 +0,0 @@
-import * as Backbone from 'backbone';
-
-export default Backbone.Events;
---
+++ src/js/helpers/messenger.js
@@ -0,0 +1,6 @@
+import * as _ from 'underscore';
+import * as Backbone from 'backbone';
+
+let Messages = _.extend({}, Backbone.Events);
+
+export default Messages;
